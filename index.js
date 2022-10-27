387b15bea3ec54c55842650b3ff4e829958d100e
aminakhatun22 <aminak26802@gmail.com>
2022-10-27 09:17:01 +0600

    add ignore file

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 4000;
-
 const categories = require('./data/categories.json');
+const cors = require('cors');
 
+//middle-wares
+app.use(cors());
 
-app.get('/teaching-categories', (req, res) => {
+// get all data 
+app.get('/courses', (req, res) => {
     res.send(categories)
+})
+// get single data 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const category = categories.find(item => item.id == id)
+    res.send(category);
 })
 
 app.get('/', (req, res) => {
-    res.send('Teaching API Running');
+    res.send("<h1>Teaching API Running</h1>");
 });
 
 app.listen(port, () => {
-    console.log('Teaching server running on port', port)
+    console.log('Tutorio server running on port', port)
 }
 )
